b011b48898acc6fc99d41ce9bd953d01bd5b6c60
Sebastian Zasuwa <sebastianzasuwa@gmail.com>
2016-10-19 20:59:28 +0200

    Added app_model_4 and app model history

---
+++ Database Models/app_model_4.sql
@@ -0,0 +1,282 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4deb1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Czas generowania: 19 Paź 2016, 14:54
+-- Wersja serwera: 5.6.30-1
+-- Wersja PHP: 7.0.11-1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `books`
+--
+
+CREATE TABLE `books` (
+  `book_id` int(10) UNSIGNED NOT NULL,
+  `book_title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `book_author` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `book_isbn` char(13) COLLATE utf8_unicode_ci NOT NULL,
+  `book_publication_date` smallint(6) NOT NULL,
+  `book_category` enum('fic','bio','bai','ckg','his','com','cst','kds','pls','law','rel','rom','sfi','hlt') COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'fiction, biographies, business and investments, cooking, history, computers, crime stories, kids, politics, law, religion, romance, sci-fi, health',
+  `book_cover` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `book_user_author` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `bookshelf_requests`
+--
+
+CREATE TABLE `bookshelf_requests` (
+  `bookshelf_request_id` int(10) UNSIGNED NOT NULL,
+  `bookshelf_latitude` float NOT NULL,
+  `bookshelf_longitude` float NOT NULL,
+  `bookshelf_name` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `bookshelf_author` int(10) UNSIGNED NOT NULL,
+  `bookshelf_request_closing_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `bookshelf_request_votes`
+--
+
+CREATE TABLE `bookshelf_request_votes` (
+  `bookshelf_request_id` int(10) UNSIGNED NOT NULL,
+  `bookshelf_request_approved` tinyint(1) NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `bookshelves`
+--
+
+CREATE TABLE `bookshelves` (
+  `bookshelf_id` int(10) UNSIGNED NOT NULL,
+  `bookshelf_latitude` float NOT NULL,
+  `bookshelf_longitude` float NOT NULL,
+  `bookshelf_name` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `bookshelf_author` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `bookshelves_books`
+--
+
+CREATE TABLE `bookshelves_books` (
+  `bookshelf_id` int(10) UNSIGNED NOT NULL,
+  `book_id` int(10) UNSIGNED NOT NULL,
+  `book_adder` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `borrowed_books`
+--
+
+CREATE TABLE `borrowed_books` (
+  `borrow_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `bookshelf_id` int(10) UNSIGNED NOT NULL,
+  `book_id` int(10) UNSIGNED NOT NULL,
+  `borrow_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `returned_books`
+--
+
+CREATE TABLE `returned_books` (
+  `return_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `bookshelf_id` int(10) UNSIGNED NOT NULL,
+  `book_id` int(10) UNSIGNED NOT NULL,
+  `return_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `user_email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `user_password` char(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `user_auth_token` char(20) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `user_signed_in` tinyint(1) NOT NULL DEFAULT '0',
+  `user_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `user_score` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `user_creation_date` datetime NOT NULL,
+  `user_account_type` enum('u','m','a') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'u' COMMENT 'user, moderator, admin'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`),
+  ADD KEY `book_user_author` (`book_user_author`);
+
+--
+-- Indexes for table `bookshelf_requests`
+--
+ALTER TABLE `bookshelf_requests`
+  ADD PRIMARY KEY (`bookshelf_request_id`);
+
+--
+-- Indexes for table `bookshelf_request_votes`
+--
+ALTER TABLE `bookshelf_request_votes`
+  ADD UNIQUE KEY `bookshelf_request_id_2` (`bookshelf_request_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `bookshelf_request_id` (`bookshelf_request_id`);
+
+--
+-- Indexes for table `bookshelves`
+--
+ALTER TABLE `bookshelves`
+  ADD PRIMARY KEY (`bookshelf_id`);
+
+--
+-- Indexes for table `bookshelves_books`
+--
+ALTER TABLE `bookshelves_books`
+  ADD UNIQUE KEY `book_id_2` (`book_id`),
+  ADD KEY `bookshelf_id` (`bookshelf_id`),
+  ADD KEY `book_id` (`book_id`),
+  ADD KEY `book_adder` (`book_adder`);
+
+--
+-- Indexes for table `borrowed_books`
+--
+ALTER TABLE `borrowed_books`
+  ADD PRIMARY KEY (`borrow_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `bookshelf_id` (`bookshelf_id`),
+  ADD KEY `book_id` (`book_id`);
+
+--
+-- Indexes for table `returned_books`
+--
+ALTER TABLE `returned_books`
+  ADD PRIMARY KEY (`return_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `bookshelf_id` (`bookshelf_id`),
+  ADD KEY `book_id` (`book_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_email` (`user_email`),
+  ADD UNIQUE KEY `user_auth_token` (`user_auth_token`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `books`
+--
+ALTER TABLE `books`
+  MODIFY `book_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT dla tabeli `bookshelf_requests`
+--
+ALTER TABLE `bookshelf_requests`
+  MODIFY `bookshelf_request_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT dla tabeli `bookshelves`
+--
+ALTER TABLE `bookshelves`
+  MODIFY `bookshelf_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT dla tabeli `borrowed_books`
+--
+ALTER TABLE `borrowed_books`
+  MODIFY `borrow_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT dla tabeli `returned_books`
+--
+ALTER TABLE `returned_books`
+  MODIFY `return_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`book_user_author`) REFERENCES `users` (`user_id`);
+
+--
+-- Ograniczenia dla tabeli `bookshelf_request_votes`
+--
+ALTER TABLE `bookshelf_request_votes`
+  ADD CONSTRAINT `bookshelf_request_votes_ibfk_1` FOREIGN KEY (`bookshelf_request_id`) REFERENCES `bookshelf_requests` (`bookshelf_request_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bookshelf_request_votes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ograniczenia dla tabeli `bookshelves_books`
+--
+ALTER TABLE `bookshelves_books`
+  ADD CONSTRAINT `bookshelves_books_ibfk_1` FOREIGN KEY (`bookshelf_id`) REFERENCES `bookshelves` (`bookshelf_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bookshelves_books_ibfk_3` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bookshelves_books_ibfk_4` FOREIGN KEY (`book_adder`) REFERENCES `users` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Ograniczenia dla tabeli `borrowed_books`
+--
+ALTER TABLE `borrowed_books`
+  ADD CONSTRAINT `borrowed_books_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `borrowed_books_ibfk_2` FOREIGN KEY (`bookshelf_id`) REFERENCES `bookshelves` (`bookshelf_id`),
+  ADD CONSTRAINT `borrowed_books_ibfk_3` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`);
+
+--
+-- Ograniczenia dla tabeli `returned_books`
+--
+ALTER TABLE `returned_books`
+  ADD CONSTRAINT `returned_books_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `returned_books_ibfk_2` FOREIGN KEY (`bookshelf_id`) REFERENCES `bookshelves` (`bookshelf_id`),
+  ADD CONSTRAINT `returned_books_ibfk_3` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
